8bf183d66088685f10c9bfef4007f8c1408a78a5
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-06-01 09:26:58 +0200

    Building Spring message producers. closes #15

---
+++ room-notifier/src/test/java/com/springboot/bootingweb/clr/RoomNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bootingweb.clr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
